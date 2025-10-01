8739ce0b6bd2e6499fcdb79dc923052bf81d9896
Pacheco67 <afonsorodriguespacheco2009@gmail.com>
2025-10-01 11:05:48 +0100

    Add files via upload

---
+++ Trabalho de disciplinas.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{ 
+    int M,P,I,FQ,Media;
+std::cout<<"Nota de Matematica";
+std::cin>>M;
+std::cout<<"Nota de Portugues";
+std::cin>>P;
+std::cout<<"Nota de Inglês";
+std::cin>>I;
+std::cout<<"Nota de fisico-Quimica";
+std::cin>>FQ;
+Media=(M+P+I+FQ)/4;
+std::cout<<"A media é: "<<Media<<"!!!!!";
+
+return 0;
+}
+
+
+
+
+
+    
+
+ 
+    
+
