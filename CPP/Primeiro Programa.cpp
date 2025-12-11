10a79f0118f8b4dc4b7435a9ec3459d9ba53f5f4
Pacheco67 <afonsorodriguespacheco2009@gmail.com>
2025-12-11 21:59:19 +0000

    Create Primeiro Programa.cpp

---
+++ CPP/Primeiro Programa.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"o Carlos acha-se o rei da cocada branca\n";
+    std::cout<<"A Beatriz Deve achar-se melhor do que todos\n";
+
+    return 0;
+}
