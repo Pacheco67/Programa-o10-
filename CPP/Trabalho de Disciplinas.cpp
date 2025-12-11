78d90a3d53d3628b538d9a9ea5a5d5771db1f44c
Pacheco67 <afonsorodriguespacheco2009@gmail.com>
2025-12-11 21:57:09 +0000

    Create Trabalho de Disciplinas.cpp

---
+++ CPP/Trabalho de Disciplinas.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{ 
+    int M,P,I,FQ,Media;
+std::cout<<"Nota de Matematica";
+std::cin>>M;
+std::cout<<"Nota de Portugues";
+std::cin>>P;
+std::cout<<"Nota de Inglês";
+std::cin>>I;
+std::cout<<"Nota de fisico-Quimica";
+std::cin>>FQ;
+Media=(M+P+I+FQ)/4;
+std::cout<<"A media é: "<<Media<<"!!!!!";
+
+return 0;
+}
